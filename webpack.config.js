3b6e3cbc2a9a35ff78391064a921f760d89a01aa
mjthedev <i@marrionjohnson.io>
2022-09-23 18:11:15 -0600

    added sticky navbar and sass

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.scss|.css$/,
-            use: ['style-loader','css-loader',]
+            use: ['style-loader','css-loader','sass-loader']
 
         },
         {
